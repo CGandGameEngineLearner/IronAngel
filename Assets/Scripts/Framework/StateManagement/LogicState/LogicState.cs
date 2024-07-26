ab64531df76679fbf4d22197c3c9b259aa174a97
lifesize <lifesize1@qq.com>
2024-07-26 10:55:30 +0800

    Create:Project On Gitlab

--- Assets/Scripts/Framework/StateManagement/LogicState/LogicState.cs
+++ Assets/Scripts/Framework/StateManagement/LogicState/LogicState.cs
@@ -1,5 +1,77 @@
 
-public class LogicState:State
+public class LogicState
 {
-    public LogicState(int hashCode):base(hashCode){}
+    public LogicState()
+    {
+        
+    }
+    
+    public LogicState(int hashCode)
+    {
+         m_HashCode = hashCode;
+    }
+
+    public LogicState(int hashCode,LogicStateManager parent)
+    {
+        m_parent = parent == null ? null : parent;
+        m_HashCode = hashCode;
+    }
+
+    virtual public void OnStateIn()
+    {
+
+    }
+
+    virtual public void Update(float deltaTime)
+    {
+
+    }
+
+    virtual public void OnStateOut()
+    {
+        
+    }
+
+    virtual public void SetActive(bool active)
+    {
+        m_Active = active;
+    }
+    virtual public bool GetActive()
+    {
+        return m_Active;
+    }
+
+    virtual public void SetParent(LogicStateManager parent)
+    {
+        m_parent = parent;
+    }
+    virtual public LogicStateManager GetParent()
+    {
+        return m_parent;
+    }
+
+    private bool m_Active = false;
+
+    protected LogicStateManager m_parent = null;
+
+    private int m_HashCode = 0;
+
+    
+    public void SetHashCode(int hashCode)
+    {
+        m_HashCode = hashCode;
+    }
+
+   
+    public override int GetHashCode()
+    {
+        return m_HashCode;
+    }
+
+
+
+    public override bool Equals(object obj)
+    {
+        return GetType().MetadataToken == obj.GetType().MetadataToken;
+    }
 }
