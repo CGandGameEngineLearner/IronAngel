8bae2b8a5f51dfeef94a3dfbf8c8d6f259d844b7
lifesize <lifesize1@qq.com>
2024-07-27 23:38:27 +0800

    Create:AI巡逻

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -1,5 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-public class AIController
+
+public class AIController : MonoBehaviour
 {
-    private GameObject m_AICharacter;
+    private AIMovement m_AIMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AIMovement = new AIMovement(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
 }
