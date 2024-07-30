644a25daa95ac4e47f0de6599b108253a290a20f
lifesize <lifesize1@qq.com>
2024-07-30 19:27:12 +0800

    重新导入BehaviourDesigner

--- Assets/Behavior Designer/Runtime/Tasks/Actions/SendEvent.cs
+++ Assets/Behavior Designer/Runtime/Tasks/Actions/SendEvent.cs
@@ -46,10 +46,6 @@ namespace BehaviorDesigner.Runtime.Tasks
 
         public override TaskStatus OnUpdate()
         {
-            if (behaviorTree == null) {
-                return TaskStatus.Failure;
-            }
-
             // Send the event and return success
             if (argument1 == null || argument1.IsNone) {
                 behaviorTree.SendEvent(eventName.Value);
