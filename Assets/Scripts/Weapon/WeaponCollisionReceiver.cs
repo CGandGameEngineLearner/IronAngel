494996c1d081355aef0d091bdaf9a64716691de9
Chen Yu <1175195719@qq.com>
2024-08-02 19:53:16 +0800

    新增两只手武器血条beta

--- Assets/Scripts/Weapon/WeaponCollisionReceiver.cs
+++ Assets/Scripts/Weapon/WeaponCollisionReceiver.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+
+[RequireComponent(typeof(BoxCollider2D))]
 public class WeaponCollisionReceiver : NetworkBehaviour
 {
     [Tooltip("这个脚本的物体是左手的武器还是右手的武器")]
     public WeaponCollisionType m_Type = WeaponCollisionType.Left;
 
+    public BoxCollider2D m_Collider;
+
+    private AmmunitionCollisionReceiver m_AmmunitionCollisionReceiver;
+
     private void Start()
     {
-        
+        m_AmmunitionCollisionReceiver = transform.parent.GetComponent<AmmunitionCollisionReceiver>();
+
+        m_Collider = GetComponent<BoxCollider2D>();
+
+        if(m_Type == WeaponCollisionType.Left)
+        {
+            if(m_AmmunitionCollisionReceiver.m_LeftWeapon)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("在相同的手部位置已经有武器了");
+#endif
+            }
+            else
+            {
+                m_AmmunitionCollisionReceiver.m_LeftWeapon = this;
+            }
+        }
+        else if(m_Type == WeaponCollisionType.Right)
+        {
+            if (m_AmmunitionCollisionReceiver.m_RightWeapon)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("在相同的手部位置已经有武器了");
+#endif
+            }
+            else
+            {
+                m_AmmunitionCollisionReceiver.m_RightWeapon = this;
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
