35bde802b5a37368ef8ef4aa48e343e4dc4ae6a9
lifesize <lifesize1@qq.com>
2024-08-10 17:58:17 +0800

    Update Buff.cs

--- Assets/Scripts/Framework/StateManagement/LogicState/Buff.cs
+++ Assets/Scripts/Framework/StateManagement/LogicState/Buff.cs
@@ -1,5 +1,8 @@
 namespace LogicState
 {
+    /// <summary>
+    /// 所有buff的基类
+    /// </summary>
     public class Buff:LogicState
     {
         public Buff(ELogicState stateEnum):base(stateEnum){}
