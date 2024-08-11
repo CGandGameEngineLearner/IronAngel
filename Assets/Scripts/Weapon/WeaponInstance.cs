65865d02ea17494f0b981c06676d2f69e773578f
LuHec <3061444633@qq.com>
2024-08-11 17:33:04 +0800

    update:fix weapon genrate

--- Assets/Scripts/Weapon/WeaponInstance.cs
+++ Assets/Scripts/Weapon/WeaponInstance.cs
@@ -96,6 +96,7 @@ public class WeaponInstance : NetworkBehaviour
         // 普通武器开火动画，激光需要特殊处理
         if (weaponType != WeaponType.CombatLaserGun && weaponType != WeaponType.HeavyLaserCannon)
         {
+            Debug.LogError(dir);
             VfxPool.Instance.GetVfx(weaponConfig.fireVfxType, startPoint, Quaternion.LookRotation(dir));
         }
         else
