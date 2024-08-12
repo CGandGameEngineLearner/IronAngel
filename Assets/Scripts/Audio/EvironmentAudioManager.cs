f3350fc1783dbd7b8393419fe882aa34a270c10a
lifesize <lifesize1@qq.com>
2024-08-13 01:44:23 +0800

    BGM测试

--- Assets/Scripts/Audio/EvironmentAudioManager.cs
+++ Assets/Scripts/Audio/EvironmentAudioManager.cs
@@ -8,7 +8,7 @@ namespace Audio
 {
     public class EvironmentAudioManager : MonoBehaviour
     {
-        public Dictionary<string, SceneEvironmentAudioSetting> m_SceneEvironmentAudioSettingDic;
+        public Dictionary<string, SceneEvironmentAudioSetting> m_SceneEvironmentAudioSettingDic = new Dictionary<string, SceneEvironmentAudioSetting>();
 
         public ScenesEvironmentAudioConfig m_ScenesEvironmentAudioConfig;
 
@@ -42,7 +42,7 @@ namespace Audio
             string currentSceneName = SceneManager.GetActiveScene().name;
             if (!m_SceneEvironmentAudioSettingDic.ContainsKey(currentSceneName))
             {
-                Debug.LogError("关卡：" + currentSceneName + " 没配置环境音效");
+                Debug.LogWarning("关卡：" + currentSceneName + " 没配置环境音效");
                 return;
             }
             var sceneEvironmentAudioSetting = m_SceneEvironmentAudioSettingDic[currentSceneName];
