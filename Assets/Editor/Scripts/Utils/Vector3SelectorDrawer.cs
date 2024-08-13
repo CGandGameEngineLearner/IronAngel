2148fa36f94e5a731da9e397d6c34374dbba246f
LuHec <3061444633@qq.com>
2024-08-14 00:29:44 +0800

    update:关卡编辑器2.0，可以看到敌人在哪出生了

--- Assets/Editor/Scripts/Utils/Vector3SelectorDrawer.cs
+++ Assets/Editor/Scripts/Utils/Vector3SelectorDrawer.cs
@@ -12,16 +12,23 @@ public class Vector3SelectorDrawer : PropertyDrawer
         EditorGUI.BeginProperty(position, label, property);
 
         // Draw the default Vector3 field
-        Rect vectorFieldRect = new Rect(position.x, position.y, position.width - 100, position.height);
+        Rect vectorFieldRect = new Rect(position.x, position.y, position.width - 200, position.height);
         EditorGUI.PropertyField(vectorFieldRect, property, label);
 
         // Draw the "Select from Scene" button
-        Rect buttonRect = new Rect(position.x + position.width - 90, position.y, 90, position.height);
-        if (GUI.Button(buttonRect, "Select Pos"))
+        Rect selectButtonRect = new Rect(position.x + position.width - 190, position.y, 90, position.height);
+        if (GUI.Button(selectButtonRect, "Select Pos"))
         {
             isSelecting = true;
             currentProperty = property;
-            SceneView.duringSceneGui += OnSceneGUI; 
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
+
+        // Draw the "Focus" button
+        Rect focusButtonRect = new Rect(position.x + position.width - 90, position.y, 90, position.height);
+        if (GUI.Button(focusButtonRect, "Focus"))
+        {
+            FocusSceneViewOnPoint(property.vector3Value);
         }
 
         EditorGUI.EndProperty();
@@ -33,22 +40,27 @@ public class Vector3SelectorDrawer : PropertyDrawer
 
         if (isSelecting && e.type == UnityEngine.EventType.MouseDown && e.button == 0)
         {
-            Vector3 mousePosition = e.mousePosition;
-            mousePosition.y = SceneView.currentDrawingSceneView.camera.pixelHeight - mousePosition.y; // Invert y axis
-            Vector3 worldPosition = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(mousePosition);
-            worldPosition.z = 0; // Ensure z is always 0
+            Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                currentProperty.vector3Value = hit.point;
+                currentProperty.serializedObject.ApplyModifiedProperties();
 
-            currentProperty.vector3Value = worldPosition;
-            currentProperty.serializedObject.ApplyModifiedProperties();
-
-            isSelecting = false;
-            SceneView.duringSceneGui -= OnSceneGUI;
-            e.Use();
+                isSelecting = false;
+                SceneView.duringSceneGui -= OnSceneGUI;
+                e.Use();
+            }
         }
         else if (e.type == UnityEngine.EventType.MouseDown && e.button != 0)
         {
             isSelecting = false;
             SceneView.duringSceneGui -= OnSceneGUI;
         }
+    }
+
+    private void FocusSceneViewOnPoint(Vector3 point)
+    {
+        SceneView.lastActiveSceneView.LookAt(point, SceneView.lastActiveSceneView.rotation, 10f);
+        SceneView.lastActiveSceneView.Repaint();
     }
 }
